a7c96cb869b56338c3be22143870682e44f74b2e
LeOral0526 <lr05267688@priestley.ac.uk>
2025-11-26 13:50:37 +0000

    Update 2nd Linear Search.py

--- 2nd Linear Search.py
+++ 2nd Linear Search.py
@@ -1,18 +1,20 @@
 import random # Imports random numbers
 
-items = [i for i in range(100)] # all numbers between 1-100
+items = [random.randint(0, 1000) for i in range(1000)] # all numbers between 1-100
 
 random.shuffle(items) # Shuffles the 100 numbers into a random order
 
 SearchTerm = int(input("Enter a number you'd like to search for: ")) # Allows the user to input a number to find in the list
 found = False
+count = 0
 
 for x in range(0, len(items)): # Loops through every number in the list
     if (SearchTerm == items[x]): # checks to see if the number has been found in the list
         found = True
+        count = count + 1
 
 
 if (found == True):
-    print("Item found")
+    print("Item found", count, "times")
 else:
     print("Item not found")
